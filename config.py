ff8df87c3d086102db9f600920a0b706dc2a0cc1
JEngelking <jengelking13@gmail.com>
2017-07-01 03:22:47 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+#dummy information used here for privacy purposes: if code is used, personal information for their app on Reddit + login information
+#should be put in this file instead.
+
+username = "myUsername"
+password = "myPassword"
+client_id = "myClientID"
+client_secret = "mySecret"
